6ee6572cfb7fa309abbbfcef91dcd8c76132446b
Michael Hashimoto <mike.hashi@liferay.com>
2023-08-20 11:54:33 -0700

    Add simple event handlers on the images

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,21 +4,45 @@ const images = [
 	{ name: "Niko 03", src: "./images/niko/niko-03.jpg" },
 	{ name: "Niko 04", src: "./images/niko/niko-04.jpg" },
 	{ name: "Little Big Awesome 01", src: "./images/little-big-awesome/little-big-awesome-01.jpg" },
-	{ name: "Little Big Awesome 02", src: "./images/little-big-awesome/little-big-awesome-02.jpeg" },
-	{ name: "Little Big Awesome 03", src: "./images/little-big-awesome/little-big-awesome-03.jpeg" },
-	{ name: "Little Big Awesome 04", src: "./images/little-big-awesome/little-big-awesome-04.jpeg" },
+	{
+		name: "Little Big Awesome 02",
+		src: "./images/little-big-awesome/little-big-awesome-02.jpeg",
+	},
+	{
+		name: "Little Big Awesome 03",
+		src: "./images/little-big-awesome/little-big-awesome-03.jpeg",
+	},
+	{
+		name: "Little Big Awesome 04",
+		src: "./images/little-big-awesome/little-big-awesome-04.jpeg",
+	},
 	{ name: "Little Big Awesome 05", src: "./images/little-big-awesome/little-big-awesome-05.png" },
 	{ name: "Little Big Awesome 06", src: "./images/little-big-awesome/little-big-awesome-06.png" },
 	{ name: "Little Big Awesome 07", src: "./images/little-big-awesome/little-big-awesome-07.png" },
 ];
+
+// Event Handler
+const handleClick = (event) => {
+	console.log(event);
+};
 
 function Main() {
 	return (
 		<main className="main">
 			<section className="images">
 				{images &&
-					images.map((image) => {
-						return <img key={image.name} src={image.src} alt={image.name} />;
+					images.map((image, index) => {
+						return (
+							<img
+								alt={image.name}
+								key={image.name}
+								onClick={(event) => {
+									handleClick(event);
+								}}
+								onMouseDown={handleClick}
+								src={image.src}
+							/>
+						);
 					})}
 			</section>
 		</main>
