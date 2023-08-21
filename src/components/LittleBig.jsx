1c9261bf3cf0dbdea852222b3f113ff3eac62cba
Michael Hashimoto <mike.hashi@liferay.com>
2023-08-20 18:02:18 -0700

    Move the Header into an overall Layout

--- src/components/LittleBig.jsx
+++ src/components/LittleBig.jsx
@@ -3,44 +3,41 @@ import Main from "./Main";
 
 function LittleBig() {
 	return (
-		<>
-			<Header />
-			<Main
-				images={[
-					{
-						name: "Little Big Awesome 01",
-						src: "./images/little-big-awesome/little-big-awesome-01.jpg",
-					},
-					{
-						name: "Little Big Awesome 02",
-						src: "./images/little-big-awesome/little-big-awesome-02.jpeg",
-					},
-					{
-						name: "Little Big Awesome 03",
-						src: "./images/little-big-awesome/little-big-awesome-03.jpeg",
-					},
-					{
-						name: "Little Big Awesome 04",
-						src: "./images/little-big-awesome/little-big-awesome-04.jpeg",
-					},
-					{
-						name: "Little Big Awesome 05",
-						src: "./images/little-big-awesome/little-big-awesome-05.png",
-					},
-					{
-						name: "Little Big Awesome 06",
-						src: "./images/little-big-awesome/little-big-awesome-06.png",
-					},
-					{
-						name: "Little Big Awesome 07",
-						src: "./images/little-big-awesome/little-big-awesome-07.png",
-					},
-				]}
-				onSelectItem={(value) => {
-					console.log(value);
-				}}
-			/>
-		</>
+		<Main
+			images={[
+				{
+					name: "Little Big Awesome 01",
+					src: "./images/little-big-awesome/little-big-awesome-01.jpg",
+				},
+				{
+					name: "Little Big Awesome 02",
+					src: "./images/little-big-awesome/little-big-awesome-02.jpeg",
+				},
+				{
+					name: "Little Big Awesome 03",
+					src: "./images/little-big-awesome/little-big-awesome-03.jpeg",
+				},
+				{
+					name: "Little Big Awesome 04",
+					src: "./images/little-big-awesome/little-big-awesome-04.jpeg",
+				},
+				{
+					name: "Little Big Awesome 05",
+					src: "./images/little-big-awesome/little-big-awesome-05.png",
+				},
+				{
+					name: "Little Big Awesome 06",
+					src: "./images/little-big-awesome/little-big-awesome-06.png",
+				},
+				{
+					name: "Little Big Awesome 07",
+					src: "./images/little-big-awesome/little-big-awesome-07.png",
+				},
+			]}
+			onSelectItem={(value) => {
+				console.log(value);
+			}}
+		/>
 	);
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,27 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
-import Home from "./components/Home";
 import LittleBig from "./components/LittleBig";
 import Niko from "./components/Niko";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import { HashRouter, Outlet, Routes, Route } from "react-router-dom";
+
+function Layout() {
+	return (
+		<>
+			<Header />
+			<Outlet />
+		</>
+	);
+}
 
 function App() {
 	return (
 		<HashRouter>
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/niko" element={<Niko />} />
-				<Route path="/little-big" element={<LittleBig />} />
+				<Route path="/" element={<Layout />}>
+					<Route path="/niko" element={<Niko />} />
+					<Route path="/little-big" element={<LittleBig />} />
+				</Route>
 			</Routes>
 		</HashRouter>
 	);
